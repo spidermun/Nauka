4a6e857bc3fea8f9cd66310bf30d987b867c0a7a
Michał <mtrela2222@gmail.com>
2025-05-28 14:10:41 +0200

    Nauka_git:Komendy

---
+++ nauka.py
@@ -0,0 +1,16 @@
+# najwazniejsze inormacje o GIT/komendy
+# git status
+# git add .  # dodaje wszystkie zmiany do stage
+# git commit -m "komentarz"  # tworzy commit z komentarzem
+# git push origin main  # wysyla zmiany na zdalne repozytorium
+# git pull origin main  # pobiera zmiany z zdalnego repozytorium
+# git branch  # pokazuje listę gałęzi
+# git checkout -b nazwa_gałęzi  # tworzy nową gałąź i przechodzi na nią
+# git merge nazwa_gałęzi  # scala zmiany z innej gałęzi do bieżącej
+# git log  # pokazuje historię commitów
+# git reset --hard HEAD  # resetuje zmiany do ostatniego commita
+# git stash  # tymczasowo zapisuje zmiany, aby móc przełączyć się na inną gałąź
+# git stash pop  # przywraca ostatnio zapisane zmiany ze stash
+# git remote add origin
+# git remote -v  # pokazuje zdalne repozytoria
+
