d2d092c8ed1a0a72fd6cd9c91606b69e403f4b31
Michał <mtrela2222@gmail.com>
2026-01-11 19:27:38 +0100

    jakies uczenie sie roznych rzeczy

---
+++ syf/type_hints.py
@@ -0,0 +1,12 @@
+# def func(param: list[str]) -> list[str]:
+#     param.reverse()
+#
+#     return param
+# print(func(["1","2"]))
+
+imie: str = "Michal"
+wiek: int = "11"
+
+wiadomosc: str = f"{imie} ma {wiek} lat"
+
+print(wiadomosc)
